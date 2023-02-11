fe825a8e74f53f93eb1d118d77ad69557aa23300
Tsuy <rafaeldesenhos99@gmail.com>
2023-02-11 14:50:25 -0300

    Feat: Create basic header

--- mobile/src/Components/Header/index.tsx
+++ mobile/src/Components/Header/index.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { Box, Text } from 'native-base';
+import React, { useCallback } from 'react';
+import { Box, Image, Text } from 'native-base';
 import { useAuth } from '../../hooks/auth';
+import { TouchableOpacity } from 'react-native';
 
 export const Header: React.FC = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+
+  const handleGoToAvatar = useCallback(() => {
+    logout();
+  }, [logout]);
 
   if (!user) {
     return null;
   }
 
   return (
-    <Box height={80}>
-      <Text>Bem vindo, {user.name}</Text>
+    <Box
+      height="16"
+      background="gray.900"
+      alignItems="center"
+      flexDirection="row"
+      justifyContent="space-between"
+      padding="4"
+    >
+      <Text color="white" fontSize="md">
+        Bem vindo, <Text color="primary.400">{user.name}</Text>
+      </Text>
+
+      <TouchableOpacity onPress={handleGoToAvatar}>
+        <Image
+          borderRadius="full"
+          w="12"
+          h="12"
+          source={{ uri: user.image }}
+          alt="User image"
+        />
+      </TouchableOpacity>
     </Box>
   );
 };

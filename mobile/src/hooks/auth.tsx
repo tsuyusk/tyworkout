c27abd8cdc789303a74ec8ef22a4bad03831e989
Tsuy <rafaeldesenhos99@gmail.com>
2023-02-11 14:50:10 -0300

    Feat: Add data persistency

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -1,10 +1,12 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from '../services/api';
 
 interface User {
   id: string;
   name: string;
   email: string;
+  image: string;
 }
 
 interface AuthContextData {
@@ -21,22 +23,47 @@ interface ProviderProps {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthContextProvider: React.FC<ProviderProps> = ({ children }) => {
+  const [hasRunUseEffect, setHasRunUseEffect] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState<User | null>(null);
 
-  const login = (userData: User, token: string) => {
+  useEffect(() => {
+    (async () => {
+      const storagedUser = await AsyncStorage.getItem('@TYWorkout/User');
+      const storagedToken = await AsyncStorage.getItem('@TYWorkout/Token');
+
+      if (storagedUser && storagedToken) {
+        setUser(JSON.parse(storagedUser));
+        api.defaults.headers.authorization = JSON.parse(storagedToken);
+      }
+
+      setHasRunUseEffect(true);
+    })();
+  }, []);
+
+  const login = async (userData: User, token: string) => {
     const bearerToken = `Bearer ${token}`;
 
     api.defaults.headers.authorization = bearerToken;
+
+    await AsyncStorage.setItem('@TYWorkout/User', JSON.stringify(user));
+    await AsyncStorage.setItem('@TYWorkout/Token', JSON.stringify(bearerToken));
 
     setIsAuthenticated(true);
     setUser(userData);
   };
 
-  const logout = () => {
+  const logout = async () => {
     setIsAuthenticated(false);
     setUser(null);
+
+    await AsyncStorage.removeItem('@TYWorkout/User');
+    await AsyncStorage.removeItem('@TYWorkout/Token');
   };
+
+  if (!hasRunUseEffect) {
+    return null;
+  }
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
